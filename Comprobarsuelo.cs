d8109944cc26ac17d4be5e2ca237c0c94a7dfc0a
DanyMartinezz <90432830+DanyMartinezz@users.noreply.github.com>
2021-11-24 02:00:23 -0500

    Add files via upload

---
+++ Comprobarsuelo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Comprobarsuelo : MonoBehaviour
+////{
+  //  private Chino jugador;
+   // // Start is called before the first frame update
+  //  void Start()
+   // {
+   //     jugador= GetComponentInParent<Chino>();
+//
+  //  }
+//void OnCollisionStay2D(Collision2D col)
+//{
+ //   jugador.Grounder =true;
+//}
+
+//void OnCollisionExit2D(Collision2D col)
+//{
+ //   jugador.Grounder =false;
+//}
+
+
+   
+//}
